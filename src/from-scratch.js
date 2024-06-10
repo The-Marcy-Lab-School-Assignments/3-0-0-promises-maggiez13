12a066c6d0ac12fd2044ab78533d1c12a0f4a173
Mei Zhu <maggiez0613@gmail.com>
2024-06-10 17:05:42 -0400

    from-scratch done

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,16 +1,40 @@
-const resolvedWrapper = () => {
+const resolvedWrapper = (value) => {
+  return new Promise((resolve) => {
+    resolve(value);
+  });
+  // return Promise.resolve(value);
 };
 
-const rejectedWrapper = () => {
+const rejectedWrapper = (value) => {
+  return new Promise((resolve, reject) => {
+    reject(new Error(value));
+  });
+  // return Promise.reject(new Error(value));
 };
 
-const handleResolvedPromise = () => {
+const handleResolvedPromise = (promise) => {
+  return promise.then((value) => {
+    console.log(value);
+    return value; 
+  });
 };
 
-const handleResolvedOrRejectedPromise = () => {
+const handleResolvedOrRejectedPromise = (promise) => {
+  return promise.then((value) => {
+    console.log(value);
+    return value;
+  }).catch((error) => {
+    console.error(`Your error message was: ${error.message}`);
+    return null;
+  });
 };
 
-const pauseForMs = () => {
+const pauseForMs = (ms) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, ms);
+  });
 };
 
 module.exports = {
